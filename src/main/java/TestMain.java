2c1f84b80d7e22e4fe64a37718c81dd70d28bf7a
twknds <twknds@naver.com>
2022-11-16 22:43:06 +0900

    refactor: run메서드 리팩터링

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,26 +1,20 @@
 import error.Assert;
 import test.Test;
 
-import java.lang.reflect.Method;
-
 public class TestMain extends Test {
     public TestMain(String testName) {
         super(testName);
     }
     public static void main(String[] args){
         new TestMain("runTest").run();
+        new TestMain("runTest_2").run();
     }
     public void runTest(){
         long sum = 10+10;
         Assert.assertTest(sum==20);
     }
-    @Override
-    public void run(){
-        try{
-            Method method = this.getClass().getMethod(super.testName,null);
-            method.invoke(this,null);
-        } catch(Exception e){
-            throw new RuntimeException(e);
-        }
+    public void runTest_2() {
+        long minus = 100-10;
+        Assert.assertTest(minus == 90);
     }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,9 +1,23 @@
 package test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+
 public abstract class Test {
     protected String testName;
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
     public Test(String testName){
         this.testName=testName;
     }
-    public abstract void run();
+    public void run(){
+        try{
+            logger.info("{} execute ",testName);
+            Method method = this.getClass().getMethod(testName,null);
+            method.invoke(this,null);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
